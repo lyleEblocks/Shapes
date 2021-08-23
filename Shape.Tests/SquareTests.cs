8d64f5a022b4ddcccca61d775b98936d5aae4ba6
tebogomfisa <tebogo@eblocks.co.za>
2021-08-23 12:01:28 +0200

    [Tebogo] added documentation for Square

--- Shape.Tests/SquareTests.cs
+++ Shape.Tests/SquareTests.cs
@@ -12,28 +12,98 @@
  * prior written
  * permission is obtained from Eblocks Software (Pty) Ltd.
 */
-
+using System;
 using Xunit;
 
 namespace Shape
 {
     /// <summary>
-    ///     This class will test our Sqaure class.
+    ///     This class will test our Square class.
     /// </summary>
     public class SquareTests
-    {
+    {   
+        /// <summary>
+        ///     This method will test that two squares are equal.
+        /// </summary>
         [Fact]
         public void Should_Ensure_Two_Squares_Are_Equal()
         {
             // Arrange
-            Square squareA = new Square(6, 6, 6, 6);
-            Square squareB = new Square(6, 6, 6, 6);
+            Square squareA = new Square(6);
+            Square squareB = new Square(6);
             
             // Act
             bool result = squareA.Equals(squareB);
 
             // Assert
             Assert.True(result);
+        }
+
+        /// <summary>
+        ///     This method will test that two squares are not equal.
+        /// </summary>
+        [Fact]
+        public void Should_Ensure_Two_Squares_Not_Equal()
+        {
+            //Arrange
+            Square squareA = new Square(5);
+            Square squareB = new Square(7);
+
+            //Act
+            bool result = squareA.Equals(squareB);
+
+            //Assert
+            Assert.False(result);
+        }
+        
+        /// <summary>
+        ///     This method will test that an exception is thrown when the dimension of a square is negative or zero.
+        /// </summary>
+        [Fact]
+        public void Should_Throw_Exception_When_Dimension_Zero_Or_Negative()
+        {
+            //Arrange
+            
+            //Act
+            //Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            { 
+                Square squareA = new Square(-1);
+            });
+        }
+
+        /// <summary>
+        ///     This method will test that the area of a square is calculated successfully.
+        /// </summary>
+        [Fact]
+        public void Should_Calculate_Area_Of_Square_Successfully()
+        {
+            //Arrange
+            Square squareA = new Square(6);
+            int expectedResult = 36;
+
+            //Act
+            int result = squareA.CalculateArea();
+
+            //Assert
+            Assert.Equal(expectedResult, result);         
+        }
+
+        /// <summary>
+        ///    This method will test that the perimeter of a square is calculated successfully. 
+        /// </summary>
+        [Fact]
+        public void Should_Calculate_Perimeter_Of_Square_Successfully()
+        {
+            //Arrange
+            Square squareA = new Square(6);
+            int expectedResult = 24;
+
+            //Act
+            int result = squareA.CalculatePerimeter();
+
+            //Assert
+            Assert.Equal(expectedResult, result);
         }
     }
 }
--- Shape/Square.cs
+++ Shape/Square.cs
@@ -12,20 +12,41 @@
  * prior written
  * permission is obtained from Eblocks Software (Pty) Ltd.
 */
+using System;
 
 namespace Shape
 {
+    /// <summary>
+    ///     This class represents a Square and its dimensions.
+    /// </summary>
     public class Square
     {
+        /// <summary>
+        ///     A <see cref="int"/> that represents the sides of a square.
+        /// </summary>
         private int sideA, sideB, sideC, sideD;
-        public Square(int a, int b, int c, int d)        
+
+        /// <summary>
+        ///     This constructor method will initialize dimension into all four sides of the square.
+        /// </summary>
+        /// <param name="dimension">A parameter <see cref="int"/> that represents the dimensions of the new square.</param>
+        public Square(int dimension)        
         {
-            sideA = a;
-            sideB = b;
-            sideC = c;
-            sideD = d;
+            if(dimension <= 0)
+            {
+               throw new ArgumentOutOfRangeException("All sides of the square must be a positive number.");
+            }
+            sideA = dimension; 
+            sideB = dimension;
+            sideC = dimension;
+            sideD = dimension;
         }
 
+        /// <summary>
+        ///     This method will compare if two squares are equal.
+        /// </summary>
+        /// <param name="obj">A parameter <see cref="int"/> that represents the squares to compare.</param>
+        /// <returns>A <see cref="bool"/> that is true if squares are equal and false if squares are not equal.</returns>
         public override bool Equals(object obj)
         {
            Square b = obj as Square;
@@ -37,6 +58,24 @@ namespace Shape
            {
                return false;
            }
+        }
+
+        /// <summary>
+        ///     This method will calculate the area of a square.
+        /// </summary>
+        /// <returns>Returns <see cref="int"/> that represents the area of the square.</returns>
+        public int CalculateArea()
+        {
+            return sideA * sideB;
+        }
+
+        /// <summary>
+        ///     This method will calculate the perimeter of a square.
+        /// </summary>
+        /// <returns>Returns <see cref="int"/> that represents the perimeter of the square.</returns>
+        public int CalculatePerimeter()
+        {
+            return sideA * 4;
         }
 
         public override int GetHashCode()
